efb37bc04d7bf2ed7796aad1b35fe6b64a3b15e7
TiberiusThompson <90928483+TiberiusThompson@users.noreply.github.com>
2022-03-05 15:35:30 +0300

    bugfix

--- js/menu.js
+++ js/menu.js
@@ -4,3 +4,10 @@ window.addEventListener('DOMContentLoaded', function() {
         document.querySelectorAll('header-nav').classList.toggle('menu-swiched')
     })
 })
+
+window.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#header__close').addEventListener('click', function () {
+        document.querySelector('#header-nav').classList.toggle('menu-swiched')
+        document.querySelectorAll('header-nav').classList.toggle('menu-swiched')
+    })
+})
